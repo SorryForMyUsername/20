9a9ba7ebd66cce2610f7b6732cb966880c152583
Ilya <119899583+SorryForMyUsername@users.noreply.github.com>
2025-01-30 22:51:38 +0200

    Add files via upload

---
+++ PersonalComputer.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20_2
+{
+    /// <summary>
+    /// Персональный компьютер
+    /// </summary>
+    internal class PersonalComputer
+    {
+        /// <summary>
+        /// Модель компьютера
+        /// </summary>
+        public string model;
+
+        /// <summary>
+        /// Тактовая частота процессора
+        /// </summary>
+        public double clockFrequency;
+
+        /// <summary>
+        /// Объём оперативной памяти
+        /// </summary>
+        public int sizeOfRAM;
+
+        /// <summary>
+        /// Объём жёсткого диска
+        /// </summary>
+        public int hardDriveCapacity;
+
+        /// <summary>
+        /// Конструктор по умолчанию или без параметров
+        /// </summary>
+        public PersonalComputer() { }
+
+        /// <summary>
+        /// Конструктор с одним параметром
+        /// </summary>
+        /// <param name="model">Модель компьютера</param>
+        public PersonalComputer(string model)
+        {
+            this.model = model;
+        }
+
+        /// <summary>
+        /// Конструктор с двумя параметрами
+        /// </summary>
+        /// <param name="model">Модель компьютера</param>
+        /// <param name="clockFrequency">Тактовая частота процессора</param>
+        public PersonalComputer(string model, double clockFrequency) : this(model)
+        {
+            this.clockFrequency = clockFrequency;
+        }
+
+        /// <summary>
+        /// Конструктор с тремя параметрами
+        /// </summary>
+        /// <param name="model">Модель компьютера</param>
+        /// <param name="clockFrequency">Тактовая частота процессора</param>
+        /// <param name="sizeOfRAM">Объём оперативной памяти</param>
+        public PersonalComputer(string model, double clockFrequency, int sizeOfRAM) : this(model, clockFrequency)
+        {
+            this.sizeOfRAM = sizeOfRAM;
+        }
+
+        /// <summary>
+        /// Конструктор с четырьмя параметрами
+        /// </summary>
+        /// <param name="model">Модель компьютера</param>
+        /// <param name="clockFrequency">Тактовая частота процессора</param>
+        /// <param name="sizeOfRAM">Объём оперативной памяти</param>
+        /// <param name="hardDriveCapacity">Объём жёсткого диска</param>
+        public PersonalComputer(string model, double clockFrequency, int sizeOfRAM, int hardDriveCapacity) : this(model, clockFrequency, sizeOfRAM)
+        {
+            this.hardDriveCapacity = hardDriveCapacity;
+        }
+
+        /// <summary>
+        /// Выводит информацию о ПК на консоль
+        /// </summary>
+        public void Info()
+        {
+            Console.WriteLine($"Модель: {model}, Тактовая частота: {clockFrequency}, " +
+                $"Объём оперативной памяти: {sizeOfRAM}, Объём жёсткого диска: {hardDriveCapacity}");
+        }
+
+        /// <summary>
+        /// Метод, возвращающий строковое представление объекта.
+        /// </summary>
+        /// <returns>Информация о ПК в строковом виде</returns>
+        public override string ToString()
+        {
+            return $"Модель: {model}, Тактовая частота: {clockFrequency}, " +
+                $"Объём оперативной памяти: {sizeOfRAM}, Объём жёсткого диска: {hardDriveCapacity}";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PersonalComputer pc1 = new PersonalComputer();
+            Console.Write("Модель ПК: ");
+            pc1.model = Console.ReadLine();
+            Console.Write("Тактовая частота процессора: ");
+            pc1.clockFrequency = double.Parse(Console.ReadLine());
+            Console.Write("Объём оперативной памяти: ");
+            pc1.sizeOfRAM = int.Parse(Console.ReadLine());
+            Console.Write("Объём жёсткого диска: ");
+            pc1.hardDriveCapacity = int.Parse(Console.ReadLine());
+            pc1.Info();
+            Console.WriteLine();
+
+            PersonalComputer pc2 = new PersonalComputer("Ноутбук");
+            Console.WriteLine(pc2);
+            Console.WriteLine();
+
+            PersonalComputer pc3 = new PersonalComputer("Настольный", 3.90);
+            pc3.sizeOfRAM = 16;
+            pc3.hardDriveCapacity = 512;
+            pc3.Info();
+            Console.WriteLine();
+
+            PersonalComputer pc4 = new PersonalComputer("Моноблок", 2.60, 32);
+            Console.WriteLine(pc4);
+            Console.WriteLine();
+
+            PersonalComputer pc5 = new PersonalComputer("Планшет", 3.20, 4, 256);
+            pc5.Info();
+
+            Console.ReadKey();
+        }
+    }
+}
